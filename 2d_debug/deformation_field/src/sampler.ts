a9eb0f76eb82e37375273f9fe6fe4cdfa1a08199
kr4b <40267916+kr4b@users.noreply.github.com>
2021-03-05 15:34:02 +0100

    Add spline creation to sampler

--- 2d_debug/deformation_field/src/sampler.ts
+++ 2d_debug/deformation_field/src/sampler.ts
@@ -1,3 +1,4 @@
+import Spline from "./spline.js";
 import { vec2 } from "./vec2.js";
 
 class Sampler<T> {
@@ -6,22 +7,24 @@ class Sampler<T> {
   sampler: Array<boolean>;
   data:    Array<T>;
   size:    number;
+  make_spline: { (t: T): Spline };
 
-  constructor(aabbMin: vec2, aabbMax: vec2, sampler: Array<boolean>, data: Array<T>, size: number) {
+  constructor(aabbMin: vec2, aabbMax: vec2, sampler: Array<boolean>, data: Array<T>, size: number, make_spline: { (t: T): Spline }) {
     this.aabbMin = aabbMin;
     this.aabbMax = aabbMax;
     this.sampler = sampler;
     this.data    = data;
     this.size    = size;
+    this.make_spline = make_spline
   }
 
-  get(samplePos: vec2): T | null {
+  get(samplePos: vec2): Spline | null {
     const index: number = Math.round(samplePos.y * this.size) + Math.round(samplePos.x);
     if (this.sampler[index]) {
       return null;
     }
 
-    return this.data[index];
+    return this.make_spline(this.data[index]);
   }
 }
 
