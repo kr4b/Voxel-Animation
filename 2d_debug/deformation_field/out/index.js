aeac5977ce96be4c957d79c16a970a1144124753
Vincent <33351005+slotho@users.noreply.github.com>
2021-03-12 11:41:33 +0100

    Add 1D debug view for rays and splines

--- 2d_debug/deformation_field/out/index.js
+++ 2d_debug/deformation_field/out/index.js
@@ -2,14 +2,22 @@ import { AABB } from "./aabb.js";
 import { Ray } from "./ray.js";
 import { Sampler } from "./sampler.js";
 import Spline from "./spline.js";
-import { add, scale, vec2 } from "./vec2.js";
+import { add, divide, max, min, scale, subtract, vec2 } from "./vec2.js";
 var atan2 = Math.atan2, cos = Math.cos, PI = Math.PI, sin = Math.sin, sqrt = Math.sqrt;
+var original_1d;
+var transformed_1d;
 var canvas;
 var ctx;
 var sampler;
 var halfFieldOfView = PI / 3;
 var camera_position = vec2(-3, 0);
 var camera_rotation = atan2(3, -0);
+var aabbColors = [
+    [255, 0, 0],
+    [0, 255, 0],
+    [0, 0, 255],
+    [200, 200, 0]
+];
 var aabbMin = vec2(-0.15, -0.15);
 var aabbMax = vec2(0.15, 0.15);
 onload = function () {
@@ -17,6 +25,16 @@ onload = function () {
     ctx = canvas.getContext("2d");
     canvas.width = 400;
     canvas.height = 400;
+    original_1d = document.getElementById("1d_og").getContext("2d");
+    original_1d.canvas.width = 400;
+    original_1d.canvas.height = 40;
+    original_1d.scale(1, original_1d.canvas.height / 2);
+    original_1d.translate(0, 1);
+    transformed_1d = document.getElementById("1d_tf").getContext("2d");
+    transformed_1d.canvas.width = 400;
+    transformed_1d.canvas.height = 40;
+    transformed_1d.scale(1, transformed_1d.canvas.height / 2);
+    transformed_1d.translate(0, 1);
     sampler = new Sampler(new AABB(scale(aabbMin, 2), scale(aabbMax, 2)), new AABB(aabbMin, aabbMax), [true, true, true, true], [[0, 1], [1, 0], [-1, 0], [0, -1]], [[255, 0, 0], [0, 255, 0], [0, 0, 255], [200, 200, 0]], function (ray, t, c) {
         var P1 = ray.origin;
         var P2 = add(P1, ray.dir);
@@ -30,20 +48,32 @@ onload = function () {
     ctx.translate(1, 1);
     ctx.lineWidth = 0.0075;
     canvas.onmousemove = function (e) {
-        var x = e.pageX / canvas.width * 2 - 1;
-        var y = e.pageY / canvas.height * 2 - 1;
+        var x = (e.pageX - canvas.offsetLeft) / canvas.width * 2 - 1;
+        var y = (e.pageY - canvas.offsetTop) / canvas.height * 2 - 1;
         camera_position = vec2(x, y);
         camera_rotation = atan2(-y, -x);
         render();
     };
     render();
 };
+function get_interpolated_color(alpha) {
+    var y_colors = [
+        aabbColors[0].map(function (v, i) { return Math.floor(aabbColors[2][i] * alpha.y + aabbColors[0][i] * (1 - alpha.y)); }),
+        aabbColors[1].map(function (v, i) { return Math.floor(aabbColors[3][i] * alpha.y + aabbColors[1][i] * (1 - alpha.y)); })
+    ];
+    var color = y_colors[0].map(function (v, i) { return Math.floor(y_colors[1][i] * alpha.x + y_colors[0][i] * (1 - alpha.x)); });
+    return "rgb(" + color[0] + ", " + color[1] + ", " + color[2] + ")";
+}
 function render() {
+    original_1d.clearRect(0, -1, original_1d.canvas.width, 2);
+    transformed_1d.clearRect(0, -1, original_1d.canvas.width, 2);
     ctx.clearRect(-1, -1, 2, 2);
     sampler.draw(ctx);
     var rays = document.querySelector("#rays").checked;
     var splines = document.querySelector("#splines").checked;
-    for (var i = -halfFieldOfView; i < halfFieldOfView; i += 0.1) {
+    var increment = 0.1;
+    var pixel_width = 400 / (halfFieldOfView * 2 / increment);
+    for (var i = -halfFieldOfView; i < halfFieldOfView; i += increment) {
         var distance = sqrt(Math.pow(camera_position.x, 2) + Math.pow(camera_position.y, 2)) * 2;
         var direction = vec2(cos(camera_rotation + i), sin(camera_rotation + i));
         var ray = new Ray(camera_position, scale(direction, distance));
@@ -54,6 +84,15 @@ function render() {
         }
         var spline = ray.intersect_ray_sampler(sampler);
         var result = spline === null || spline === void 0 ? void 0 : spline.intersect_spline_aabb(sampler.realAABB);
+        var ts = ray.intersect_ray_aabb(new AABB(aabbMin, aabbMax));
+        if (ts.x <= ts.y && ts.y >= 0.0) {
+            // Draw the view with the rays
+            var worldEntry = add(ray.origin, scale(ray.dir, ts.x));
+            var vscale = subtract(aabbMax, aabbMin);
+            var ventry = min(vec2(1, 1), max(vec2(0, 0), divide(subtract(worldEntry, aabbMin), vscale)));
+            original_1d.fillStyle = get_interpolated_color(ventry);
+            original_1d.fillRect((i + halfFieldOfView) * pixel_width / increment, -1, pixel_width, 2);
+        }
         if (splines && spline != null) {
             spline.draw(ctx);
         }
@@ -62,6 +101,11 @@ function render() {
             spline === null || spline === void 0 ? void 0 : spline.draw_point_at(ctx, spline.ts.x);
             spline === null || spline === void 0 ? void 0 : spline.draw_point_at(ctx, spline.ts.y);
             ctx.fillStyle = "black";
+            var worldEntry = spline.position_on_spline(ts.x);
+            var vscale = subtract(aabbMax, aabbMin);
+            var ventry = min(vec2(1, 1), max(vec2(0, 0), divide(subtract(worldEntry, aabbMin), vscale)));
+            transformed_1d.fillStyle = get_interpolated_color(ventry);
+            transformed_1d.fillRect((i + halfFieldOfView) * pixel_width / increment, -1, pixel_width, 2);
         }
     }
     ctx.strokeStyle = "gray";
--- 2d_debug/deformation_field/src/index.ts
+++ 2d_debug/deformation_field/src/index.ts
@@ -2,9 +2,12 @@ import { AABB } from "./aabb.js";
 import { Ray } from "./ray.js";
 import { Sampler } from "./sampler.js";
 import Spline from "./spline.js";
-import { add, multiply, scale, vec2 } from "./vec2.js";
+import { add, divide, max, min, multiply, scale, subtract, vec2 } from "./vec2.js";
 
 const { atan2, cos, PI, sin, sqrt } = Math;
+
+let original_1d: CanvasRenderingContext2D;
+let transformed_1d: CanvasRenderingContext2D;
 
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
@@ -13,6 +16,12 @@ const halfFieldOfView = PI / 3;
 let camera_position = vec2(-3, 0);
 let camera_rotation = atan2(3, -0);
 
+const aabbColors = [ 
+    [ 255, 0, 0 ],
+    [ 0, 255, 0 ],
+    [ 0, 0, 255 ],
+    [ 200, 200, 0]
+];
 const aabbMin = vec2(-0.15, -0.15);
 const aabbMax = vec2(0.15, 0.15);
 
@@ -21,6 +30,19 @@ onload = () => {
     ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
     canvas.width = 400;
     canvas.height = 400;
+
+    original_1d = <CanvasRenderingContext2D> (<HTMLCanvasElement> document.getElementById("1d_og")).getContext("2d");
+    original_1d.canvas.width = 400;
+    original_1d.canvas.height = 40;
+    original_1d.scale(1, original_1d.canvas.height / 2);
+    original_1d.translate(0, 1);
+
+    transformed_1d = <CanvasRenderingContext2D> (<HTMLCanvasElement> document.getElementById("1d_tf")).getContext("2d");
+    transformed_1d.canvas.width = 400;
+    transformed_1d.canvas.height = 40;
+    transformed_1d.scale(1, transformed_1d.canvas.height / 2);
+    transformed_1d.translate(0, 1);
+
 
     sampler = new Sampler(
         new AABB(scale(aabbMin, 2), scale(aabbMax, 2)),
@@ -46,8 +68,8 @@ onload = () => {
     ctx.lineWidth = 0.0075;
 
     canvas.onmousemove = e => {
-        const x = e.pageX / canvas.width * 2 - 1;
-        const y = e.pageY / canvas.height * 2 - 1;
+        const x = (e.pageX - canvas.offsetLeft) / canvas.width * 2 - 1;
+        const y = (e.pageY - canvas.offsetTop) / canvas.height * 2 - 1;
 
         camera_position = vec2(x, y);
         camera_rotation = atan2(-y, -x);
@@ -58,13 +80,28 @@ onload = () => {
     render();
 }
 
+function get_interpolated_color(alpha: vec2) {
+    const y_colors = [
+        aabbColors[0].map((v, i) => Math.floor(aabbColors[2][i] * alpha.y + aabbColors[0][i] * (1 - alpha.y))),
+        aabbColors[1].map((v, i) => Math.floor(aabbColors[3][i] * alpha.y + aabbColors[1][i] * (1 - alpha.y)))
+    ];
+    const color = y_colors[0].map((v, i) => Math.floor(y_colors[1][i] * alpha.x + y_colors[0][i] * (1 - alpha.x)));
+
+    return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+}
+
 function render() {
+    original_1d.clearRect(0, -1, original_1d.canvas.width, 2);
+    transformed_1d.clearRect(0, -1, original_1d.canvas.width, 2);
+
     ctx.clearRect(-1, -1, 2, 2);
     sampler.draw(ctx);
     const rays: boolean = (document.querySelector("#rays") as HTMLInputElement).checked;
     const splines: boolean = (document.querySelector("#splines") as HTMLInputElement).checked;
 
-    for (let i = -halfFieldOfView; i < halfFieldOfView; i += 0.1) {
+    const increment = 0.1;
+    const pixel_width = 400 / (halfFieldOfView * 2 / increment);
+    for (let i = -halfFieldOfView; i < halfFieldOfView; i += increment) {
         const distance:  number = sqrt(camera_position.x ** 2 + camera_position.y ** 2) * 2;
         const direction: vec2   = vec2(cos(camera_rotation + i), sin(camera_rotation + i));
         const ray:       Ray    = new Ray(camera_position, scale(direction, distance));
@@ -78,6 +115,17 @@ function render() {
         const spline: Spline | null = ray.intersect_ray_sampler(sampler);
         const result: boolean | undefined = spline?.intersect_spline_aabb(sampler.realAABB);
 
+        const ts: vec2 = ray.intersect_ray_aabb(new AABB(aabbMin, aabbMax));
+        if (ts.x <= ts.y && ts.y >= 0.0) {
+            // Draw the view with the rays
+            const worldEntry: vec2 = add(ray.origin, scale(ray.dir, ts.x));
+            const vscale: vec2 = subtract(aabbMax, aabbMin);
+            const ventry: vec2 = min(vec2(1, 1), max(vec2(0, 0), divide(subtract(worldEntry, aabbMin), vscale)));
+
+            original_1d.fillStyle = get_interpolated_color(ventry);
+            original_1d.fillRect((i + halfFieldOfView) * pixel_width / increment, -1, pixel_width, 2);
+        }
+
         if (splines && spline != null) {
             spline.draw(ctx);
         }
@@ -87,6 +135,13 @@ function render() {
             spline?.draw_point_at(ctx, spline.ts.x);
             spline?.draw_point_at(ctx, spline.ts.y);
             ctx.fillStyle = "black";
+
+            const worldEntry: vec2 = (<Spline> spline).position_on_spline(ts.x);
+            const vscale: vec2 = subtract(aabbMax, aabbMin);
+            const ventry: vec2 = min(vec2(1, 1), max(vec2(0, 0), divide(subtract(worldEntry, aabbMin), vscale)));
+
+            transformed_1d.fillStyle = get_interpolated_color(ventry);
+            transformed_1d.fillRect((i + halfFieldOfView) * pixel_width / increment, -1, pixel_width, 2);
         }
     }
 
