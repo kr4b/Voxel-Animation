b839a2bee527fa20aed0206da5ce662447a4fd92
kr4b <40267916+kr4b@users.noreply.github.com>
2021-07-04 16:46:21 +0200

    Fix spline map texture coordinate calculation

--- 2d_debug/spline_map/src/spline_map.ts
+++ 2d_debug/spline_map/src/spline_map.ts
@@ -2,31 +2,54 @@ import { AABB } from "./aabb.js";
 import { Plane } from "./plane.js";
 import { Ray } from "./ray.js";
 import Spline from "./spline.js";
-import { add, dot, multiply, scale, subtract, vec2 } from "./vec2.js";
+import { add, dot, length, max, min, multiply, scale, subtract, vec2 } from "./vec2.js";
 
 class SplineMap {
   aabb: AABB;
   base: Plane;
   spline: Spline;
 
-  constructor(aabb: AABB, base: Plane, spline: Spline) {
-    this.aabb = aabb;
+  constructor(base: Plane, spline: Spline) {
     this.base = base;
     this.spline = spline;
+    // Create the encompassing AABB
+    const extremes = Array.from(spline.get_extremes().filter(t => t >= 0 && t <= 1)).map(t => spline.position_on_spline(t));
+    this.aabb = new AABB(
+      min(...extremes),
+      add(base.size, max(...extremes)),
+    );
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.strokeStyle = "salmon";
+    this.aabb.draw(ctx);
+    ctx.strokeStyle = "olive";
+    this.spline.draw(ctx);
+    ctx.save();
+    ctx.translate(this.base.size.x, 0.0);
+    this.spline.draw(ctx);
+    ctx.restore();
+
+    ctx.beginPath();
+    ctx.moveTo(this.base.center.x - this.base.half_size.x, this.base.center.y - this.base.half_size.y);
+    ctx.lineTo(this.base.center.x + this.base.half_size.x, this.base.center.y - this.base.half_size.y);
+    ctx.moveTo(this.base.center.x - this.base.half_size.x, this.base.center.y + this.base.size.x);
+    ctx.lineTo(this.base.center.x + this.base.half_size.x, this.base.center.y + this.base.size.x);
+    ctx.stroke();
   }
 
   intersect_ray(ray: Ray): vec2 {
     return ray.intersect_ray_aabb(this.aabb);
   }
 
-  // Calculate texture coords for a given position in world space
+  /// Calculate texture coords for a given position in world space
   texture_coords(pos: vec2): vec2 | null {
     // Create base plane at the given position
     // TODO: use infinite plane instead of scaling
     const plane = new Plane(pos, this.base.size);
     // Get plane spline intersection
     // TODO: This check can probably be skipped when using an infinite plane
-    if (this.spline.intersect_spline_plane(plane)) {
+    if (!this.spline.intersect_spline_plane(plane)) {
       return null;
     }
     const t = this.spline.ts.x;
@@ -41,7 +64,10 @@ class SplineMap {
     }
 
     // Calculate and return texture coords
-    return subtract(pos, edgePos1);
+    // x component is distance to base spline, scaled by base length
+    // y component is currently asumed to be t
+    const xComp = length(subtract(pos, edgePos1)) / length(this.base.size);
+    return vec2(xComp, t);
   }
 }
 
