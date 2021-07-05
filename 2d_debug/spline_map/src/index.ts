1e9b25af76c08ddf58a61a246b5ca79a572dbb73
Vincent <33351005+slotho@users.noreply.github.com>
2021-07-05 10:15:21 +0200

    Add 1d view

--- 2d_debug/spline_map/src/index.ts
+++ 2d_debug/spline_map/src/index.ts
@@ -8,6 +8,11 @@ import { mat3, norm, scale, vec2 } from "./vec2.js";
 
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
+let transformed_1d: CanvasRenderingContext2D;
+
+let camera_position = vec2(-0.8, 0);
+let camera_rotation = Math.atan2(0, 0.8);
+const halfFieldOfView = Math.PI / 3;
 
 const EPSILON = 1e-6;
 
@@ -19,8 +24,8 @@ const pixels = [
 ];
 
 const size = Math.sqrt(pixels.length);
-const aabbMin = vec2(-0.35, -0.35);
-const aabbMax = vec2(0.35, 0.35);
+const aabbMin = vec2(-0.25, -0.25);
+const aabbMax = vec2(0.25, 0.25);
 const canvasWidth = 400;
 const canvasHeight = 400;
 const pixelSizeX = 2.0 / canvasWidth;
@@ -33,12 +38,26 @@ onload = () => {
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
 
+    canvas.onmousemove = e => {
+        const x = (e.pageX - canvas.offsetLeft) / canvas.width * 2 - 1;
+        const y = (e.pageY - canvas.offsetTop) / canvas.height * 2 - 1;
+
+        camera_position = vec2(x, y);
+        camera_rotation = Math.atan2(-y, -x);
+    }
+
     ctx.scale(canvas.width / 2, canvas.height / 2);
     ctx.translate(1, 1);
     ctx.lineWidth = pixelSizeX * 2.0;
 
-    // const base: Plane = new Plane(vec2(aabbMin.x + 0.5 * (aabbMax.x - aabbMin.x), aabbMin.y), scale(vec2(aabbMax.x - aabbMin.x, 0), 0.5));
-    const base: Plane = new Plane(vec2(0.1, -0.2), vec2(0.5, 0.3));
+    transformed_1d = <CanvasRenderingContext2D>(<HTMLCanvasElement>document.getElementById("1d_tf")).getContext("2d");
+    transformed_1d.canvas.width = 400;
+    transformed_1d.canvas.height = 40;
+    transformed_1d.scale(1, transformed_1d.canvas.height / 2);
+    transformed_1d.translate(0, 1);
+
+    const base: Plane = new Plane(vec2(aabbMin.x + 0.5 * (aabbMax.x - aabbMin.x), aabbMin.y), scale(vec2(aabbMax.x - aabbMin.x, 0), 0.5));
+    // const base: Plane = new Plane(vec2(0.1, -0.2), scale(norm(vec2(0.5, 0.3)), aabbMax.x - aabbMin.x));
 
     // const spline = Spline.with_control_points(aabbMin, vec2(aabbMin.x - 0.1, aabbMax.y), vec2(-15.0, -3.0), vec2(12.0, 0.0));
     const spline = Spline.with_control_points(vec2(0, 0), vec2(0, aabbMax.y - aabbMin.y), vec2(-15.0, -3.0), vec2(12.0, 0.0));
@@ -48,22 +67,37 @@ onload = () => {
     let t = 0;
     setInterval(() => {
         ctx.clearRect(-1, -1, 2, 2);
+        transformed_1d.clearRect(0, -1, transformed_1d.canvas.width, 2);
 
         render_texture(ctx, spline_map);
         spline_map.draw(ctx);
 
-        const ray = new Ray(vec2(Math.sin(t) * (aabbMax.x - aabbMin.x), 0.6), vec2(0.0, -1.2));
-        ray.draw(ctx);
-        const result = ray.walk_spline_map(spline_map, pixelSizeY);
-        if (result !== null) {
-            const [texCoords, t] = result;
-            const pixelX = Math.floor((Math.round(texCoords.x * 1000.0) / 1000.0 - EPSILON) * size);
-            const pixelY = Math.floor((Math.round(texCoords.y * 1000.0) / 1000.0 - EPSILON) * size);
-            if (pixelX >= 0 && pixelX < size && pixelY >= 0 && pixelY < size) {
-                const color = pixels[pixelY * size + pixelX];
-                ctx.fillStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
-                ctx.fillRect(-1.0, -1.0, 0.2, 0.2);
-                ray.draw_point_at(ctx, t);
+        const increment = 0.05;
+        const pixel_width = 400 / (halfFieldOfView * 2 / increment);
+        for (let i = -halfFieldOfView; i < halfFieldOfView; i += increment) {
+            const distance: number = Math.sqrt(camera_position.x ** 2 + camera_position.y ** 2) * 2;
+            const direction: vec2 = vec2(Math.cos(camera_rotation + i), Math.sin(camera_rotation + i));
+            const ray: Ray = new Ray(camera_position, scale(direction, distance));
+            // const ray = new Ray(vec2(Math.sin(t) * spline_map.aabb.size().x * 0.5, 0.6), vec2(0.0, -1.2));
+
+            ray.draw(ctx);
+
+            const result = ray.walk_spline_map(spline_map, pixelSizeY);
+            if (result !== null) {
+                const [texCoords, t] = result;
+                const pixelX = Math.floor((Math.round(texCoords.x * 1000.0) / 1000.0 - EPSILON) * size);
+                const pixelY = Math.floor((Math.round(texCoords.y * 1000.0) / 1000.0 - EPSILON) * size);
+
+                if (pixelX >= 0 && pixelX < size && pixelY >= 0 && pixelY < size) {
+                    const color = pixels[pixelY * size + pixelX];
+                    ctx.fillStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+                    ctx.fillRect(-1.0, -1.0, 0.2, 0.2);
+
+                    ray.draw_point_at(ctx, t);
+
+                    transformed_1d.fillStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+                    transformed_1d.fillRect((i + halfFieldOfView) * pixel_width / increment, -1, pixel_width, 2);
+                }
             }
         }
 
